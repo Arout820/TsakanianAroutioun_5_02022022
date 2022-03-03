edc475d8326a504a4eda29713ec47dc3ab89dd26
Aroutioun Tsakanian <aroutioun.tsakanian@gmail.com>
2022-03-03 20:54:38 +0100

    correction of the page confirmation

--- front/js/confirmation.js
+++ front/js/confirmation.js
@@ -3,3 +3,4 @@
 let orderId = document.querySelector("#orderId");
 let url = new URL(document.URL);
 orderId.innerText = url.searchParams.get("orderId");
+localStorage.setItem("product", JSON.stringify(new Array));

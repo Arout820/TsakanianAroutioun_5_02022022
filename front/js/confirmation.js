908071b8df792a09b13356d9c8b632a644ff310e
Aroutioun Tsakanian <aroutioun.tsakanian@gmail.com>
2022-05-15 16:33:56 +0200

    refactor: removing blank

--- front/js/confirmation.js
+++ front/js/confirmation.js
@@ -1,5 +1,4 @@
 // Recuperation et affichage du numéro de commande sur la page confirmation
-
 let orderId = document.querySelector('#orderId');
 let url = new URL(document.URL);
 orderId.innerText = url.searchParams.get('orderId');
